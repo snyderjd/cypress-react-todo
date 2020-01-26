a481fcbf7f698f2968abd8a2808b97d287cbf833
Joe Snyder <snyderjd@Joes-MacBook-Pro.local>
2020-01-26 15:15:06 -0600

    Stub POST to server and tests for successful display on page and resetting of text input

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -13,4 +13,46 @@ describe('Input form', function() {
         cy.get('.new-todo').type(typedText)
             .should('have.value', typedText)
     })
+
+    context('Form submission', function() {
+        beforeEach(() => {
+            cy.server()
+        })
+
+        it('Adds a new todo on submit', function() {
+            const itemText = 'Buy eggs'
+
+            cy.route('POST', '/api/todos', {
+                name: itemText,
+                id: 1,
+                isComplete: false
+            })
+
+            cy.get('.new-todo')
+              .type(itemText)
+              .type('{enter}')
+              .should('have.value', '')
+
+            cy.get('.todo-list li')
+                .should('have.length', 1)
+                .and('contain', itemText )
+        })
+
+        it('Shows an error message on a failed submission', function() {
+            cy.route({
+                url: '/api/todos',
+                method: 'POST',
+                status: 500,
+                response: {}
+            })
+
+            cy.get('.new-todo').type('test{enter}')
+            
+            cy.get('.todo-list li')
+                .should('not.exist')
+
+            cy.get('.error')
+                .should('be.visible')
+        })
+    })
 })
