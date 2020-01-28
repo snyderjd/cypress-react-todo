9d58ea179e323fae0aa507e8ebfb5b4b03a1e482
Joe Snyder <snyderjosephd@yahoo.com>
2020-01-28 12:38:54 -0600

    Using json-server for back-end, wrote end-to-end test for creating multiple todos

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -34,12 +34,14 @@ export default class TodoApp extends Component {
   handleTodoSubmit (event) {
     event.preventDefault()
     const newTodo = { name: this.state.currentTodo, isComplete: false }
+    
     saveTodo(newTodo)
-      .then(({data}) => this.setState({
-        todos: this.state.todos.concat(data),
-      currentTodo: ''
-      }))
-      .catch(() => this.setState({ error: true }))
+    .then(({data}) => this.setState({
+      todos: this.state.todos.concat(data),
+    currentTodo: ''
+    }))
+    .catch(() => this.setState({ error: true }))
+    
   }
 
   handleDelete (id) {
